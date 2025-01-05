8b4ceaf232b98c7a391864d514036e8631f61877
Hemant Negi <140361940+HemantNegi42@users.noreply.github.com>
2025-01-05 12:44:10 +0530

    Create 26-remove-duplicates-from-sorted-array.cpp

---
+++ Array/26-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n=nums.size();
+        int k=0;
+        for(int i=0;i<n-1;i++){
+            if(nums[i]!=nums[i+1]){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        nums[k]=nums[n-1];
+        return k+1;
+    }
+};
